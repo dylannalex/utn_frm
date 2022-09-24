e9721944e310ac6bf8696018e7498a246c077c85
Dylan Tintenfich <tintenfichdylan05@gmail.com>
2022-09-24 13:27:38 -0300

    Add race_condition.c

---
+++ third_year/parallel_computing/practice/race_condition/race_condition.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+
+int total_mails_per_thread = 100000000;
+int mails = 0;
+
+void *routine()
+{
+    int i;
+    for (i = 0; i < total_mails_per_thread; i++)
+    {
+        mails++;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    pthread_t p1, p2;
+
+    pthread_create(&p1, NULL, &routine, NULL);
+    pthread_create(&p2, NULL, &routine, NULL);
+    pthread_join(p1, NULL);
+    pthread_join(p2, NULL);
+
+    printf("Number of mails to be sent:    %d\n", total_mails_per_thread * 2);
+    printf("Number of mails actually sent: %d\n", mails);
+    return 0;
+}
